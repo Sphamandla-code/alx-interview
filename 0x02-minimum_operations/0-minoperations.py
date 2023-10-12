a2f22fb236ff0d4fb947a4c6e843040e6a764d33
Sphamandla-code <nkambulesphamandla@gmail.com>
2023-10-12 15:33:32 +0200

    0x02

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+method to calculates the fewest number of operations
+needed to result in exactly n H characters in a file
+Prototype: def minOperations(n)
+Returns an integer
+if n is impossible to achieve, return 0
+"""
+
+
+def minOperations(n):
+    """
+    Function minOperations
+    Returns an integer
+    """
+    result = 0
+    x = 2
+    while n > 1:
+        while n % x == 0:
+            result += x
+            n /= x
+        x += 1
+    return result
